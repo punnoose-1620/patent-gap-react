d0cb2c76ce412b71599ff4a77eff14445fa159b0
jinsypanamkattu <jinsypanamkattu@gmail.com>
2026-03-09 13:36:29 +0100

    Update axios base URL to production endpoint

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://patent-gap-backend.onrender.com/api',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
