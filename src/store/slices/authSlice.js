5fba178f165006074241615fade047dd647d2466
jinsypanamkattu <jinsypanamkattu@gmail.com>
2026-03-09 12:38:32 +0100

    updated the authentication slice

---
+++ src/store/slices/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    isAuthenticated: false,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    register: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { login, register, logout } = authSlice.actions;
+export default authSlice.reducer;
