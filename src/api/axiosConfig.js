dc1e7ee2b686d8b2f7bfb1a42b0fe36373553eb8
Punnoose K Thomas <punnoose1997@gmail.com>
2026-04-27 01:46:20 +0200

    Undo: Fix was meant for Main

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://backend.patentgap.ai/api',
+  baseURL: 'https://backend-dev.patentgap.ai/api',
   timeout: 600000, // 10 minutes
   //withCredentials: true,          // ← add this
   headers: {
